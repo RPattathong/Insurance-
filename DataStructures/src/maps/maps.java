059de4a90fefc79504918556412a03ddddebb671
Matt <M_Pattathong@outlook.com>
2022-05-26 23:00:04 -0400

    first commit

--- DataStructures/src/maps/maps.java
+++ DataStructures/src/maps/maps.java
@@ -49,7 +49,12 @@ public class maps {
 		
 		System.out.println(person1);
 		System.out.println(person2);
+		System.out.println(person3);
+		System.out.println(person4);
+		System.out.println(person5);
+		System.out.println(person6);
 
+// git practice
 	}
 
 }
